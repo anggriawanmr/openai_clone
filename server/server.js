456557d158132e099b1145bc9230226042c21120
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-23 01:15:17 +0700

    Editing server and the package :hammer:

--- server/server.js
+++ server/server.js
@@ -25,6 +25,25 @@ app.post('/', async (req, res) => {
   try {
     const prompt = req.body.prompt;
 
-    const response = await openai.createCompletion({});
-  } catch (error) {}
+    const response = await openai.createCompletion({
+      model: 'text-davinci-003',
+      prompt: `${prompt}`,
+      temperature: 0,
+      max_tokens: 3000,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
+
+    res.status(200).send({
+      bot: response.data.choices[0].text,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ error });
+  }
 });
+
+app.listen(5000, () =>
+  console.log('Server is running on port http://locaalhost:5000')
+);
