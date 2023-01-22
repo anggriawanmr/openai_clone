1fcd871649b403f54c45154538d76099806f4bf0
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-23 01:23:52 +0700

    Adding fetch in front end side :sparkles:

--- server/server.js
+++ server/server.js
@@ -12,10 +12,10 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const app = express();
-app.use(cores());
+app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, rese) => {
+app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from CodeX',
   });
@@ -45,5 +45,5 @@ app.post('/', async (req, res) => {
 });
 
 app.listen(5000, () =>
-  console.log('Server is running on port http://locaalhost:5000')
+  console.log('Server is running on port http://localhost:5000')
 );
