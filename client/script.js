a4ad3adae83925a8378034dadd08a91fb7cd54b6
Anggriawan Ridatama <anggriawanmr@gmail.com>
2023-01-30 09:58:58 +0700

    Editing client side for deploying :rocket:

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv);
 
   // fetch data from server => bot's responses
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://openai-clone-mocha.vercel.app/', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
